60a52927c6dc110dcbe09d23cdd2605e7bf7dbb0
Svetlana <se55petrova@gmail.com>
2022-06-21 21:48:58 +0500

    Изменила код JS для работы с несколькими скрытыми новостями

--- source/js/button-news.js
+++ source/js/button-news.js
@@ -1,16 +1,20 @@
 let newsButton=document.querySelector('.news__to-all');
-    lastNews=document.querySelector('.news__item--none');
+    hiddenNews=document.querySelectorAll('.news__item--none');
     btnAll=document.querySelector('.button--to-all');
     btnCollapse=document.querySelector('.button--collapse');
 
-newsButton.addEventListener("click", function () {
-   if (lastNews.classList.contains("news__item--none"))  {
-       lastNews.classList.remove("news__item--none");
-       btnAll.classList.add('news__button--none');
-       btnCollapse.classList.remove('news__button--none');
-   } else {
-       lastNews.classList.add("news__item--none");
-       btnAll.classList.remove('news__button--none');
-       btnCollapse.classList.add('news__button--none');
-   }
-});
+
+    newsButton.addEventListener("click", function () {
+        for (const news of hiddenNews) {
+            if (news.classList.contains("news__item--none")) {
+                news.classList.remove("news__item--none");
+                btnAll.classList.add('news__button--none');
+                btnCollapse.classList.remove('news__button--none');
+            } else {
+                news.classList.add("news__item--none");
+                btnAll.classList.remove('news__button--none');
+                btnCollapse.classList.add('news__button--none');
+                 }
+
+        }
+    });
